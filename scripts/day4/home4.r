aed27a54ca3d634ac32ef48ba87a10e830b65730
Vladimir Bogatyrov <vbogatyrov@talend.com>
2019-03-01 22:07:20 +0200

    [day 4] Home

--- scripts/day4/home4.r
+++ scripts/day4/home4.r
@@ -29,8 +29,14 @@ ggplot(aes(x = Iec, y = Is), data = df) +
   geom_point() 
 
 lm1 <- lm(Cql ~ Ie + Iec + Is, data = df)
+# lm1$coefficients
 lm2 <- lm(Cql ~ Ie + Iec, data = df)
 lm3 <- lm(Cql ~ Ie + Is, data = df)
 lm4 <- lm(Cql ~ Iec + Is, data = df)
+
+
+nls1<-nls(formula = Cql ~ a*Ie^d + b*Iec^e + c*Is^f, data=df,start=list(a=1, b=1, c=1, d=1, e=1, f=1))
+cc <- coef(nls1)
+
 
 
